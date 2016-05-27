d62c95e697ad04cfde9add6018ae23af43a72669
Zachary Wade <d4rkenedmatter@gmail.com>
2016-05-27 11:01:11 -0400

    Scene Layout and Camera Control(Started)
    Above

---
+++ Project Warfare/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed = 5F;
+    public float turnSpeed = 50F;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        keyboardMovement();
+	}
+
+    void keyboardMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        }
+    }
+    
+}
